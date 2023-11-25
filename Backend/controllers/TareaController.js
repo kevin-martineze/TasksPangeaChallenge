e883270a35155444a4f3cd673accafe85a0a8c85
Kevin Martínez <54540601+kevin-martineze@users.noreply.github.com>
2023-11-25 07:49:05 -0500

    agregando funciones a la tarea
    agregar tarea, obtener tarea, actualizar tarea

--- Backend/controllers/TareaController.js
+++ Backend/controllers/TareaController.js
@@ -1,4 +1,4 @@
-import Tarea from "../models/Tarea.js"
+import Tarea from "../models/Tarea.js";
 import Proyecto from "../models/Proyecto.js";
 
 const agregarTarea = async (req, res) => {
@@ -24,9 +24,51 @@ const agregarTarea = async (req, res) => {
   }
 };
 
-const obtenerTarea = async (req, res) => {};
+const obtenerTarea = async (req, res) => {
+  const { id } = req.params;
 
-const actualizarTarea = async (req, res) => {};
+  const tarea = await Tarea.findById(id).populate("proyecto");
+
+  if (!tarea) {
+    const error = new Error("Tarea no encontrada");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  res.json(tarea);
+};
+
+const actualizarTarea = async (req, res) => {
+  const { id } = req.params;
+
+  const tarea = await Tarea.findById(id).populate("proyecto");
+
+  if (!tarea) {
+    const error = new Error("Tarea no encontrada");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  tarea.nombre = req.body.nombre || tarea.nombre;
+  tarea.descripcion = req.body.descripcion || tarea.descripcion;
+  tarea.prioridad = req.body.prioridad || tarea.prioridad;
+  tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
+
+  try {
+    const tareaAlmacenada = await tarea.save()
+    res.json(tareaAlmacenada)
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 const eliminarTarea = async (req, res) => {};
 
